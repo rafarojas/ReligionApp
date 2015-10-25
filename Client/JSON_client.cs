776bc760dcc08e6e0ec7d4575d1724fb1ebdcef0
rafarojas <rojasrafael@gmail.com>
2015-10-25 17:17:33 -0600

    Project to Test The API

---
+++ Client/JSON_client.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class JSON_client
+    {
+        string endpoint;
+
+        public JSON_client(string endpointUrl)
+        {
+            endpoint = endpointUrl;
+        }
+
+        public void POST(string method, string JSONparameters)
+        {
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(endpoint + method);
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "text/json";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(JSONparameters);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        Console.WriteLine(method + " result: " + result.ToString());
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    Console.WriteLine("Exception caught: " + errorText);
+                }
+            }
+        }
+
+        public void GET(string method)
+        {
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(endpoint + method);
+                request.Method = "GET";
+
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    var result = reader.ReadToEnd();
+                    Console.WriteLine(method + " result: " + result.ToString());
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    Console.WriteLine("Exception caught: " + errorText);
+                }
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using CatholicAPI;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string endpointUrl = "http://localhost:59162/CatholicService.svc///";
+            string endpointUrl = "http://54.152.95.162/CatholicAPI/CatholicService.svc///";
+            string method;
+            string parameters;
+            JSON_client client = new JSON_client(endpointUrl);
+
+            //method = "FrontLineQueueTest";
+            //parameters = "{\"testTag\":\"MOBILE_LOBBY_TEST\"," + "\"casinoPrefix\":\"turmajtc\"," + "\"testUrl\":\"http://downloadtur.majesticslots.com/mobile\"}";
+
+            method = "GetEvents";
+            parameters = "";// "{\"testTag\":\"MOBILE_CASHIER_TEST\"," + "\"casinoPrefix\":\"eurmajtc\"}";
+            
+            client.POST(method, parameters);
+
+            Console.ReadLine();
+        }
+    }
+}
